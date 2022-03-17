6648b62c24287c6ceae390ba677a4e33af50de65
Niccolò <niccogazza@gmail.com>
2022-03-17 10:47:07 +0100

    Eliminati file inutili

--- MATLAB scripts/compute_vicon_vel.m
+++
@@ -1,8 +0,0 @@
-function [vicon_vel] = compute_vicon_vel(vicon_pos)
-    dt = 0.1; %[s] => vicon pos at 10Hz
-    vicon_vel = zeros(length(vicon_pos), 1);
-    for i=1:length(vicon_pos) - 1
-        vicon_vel(i+1) = (vicon_pos(i+1) - vicon_pos(i))/dt;
-    end
-end
-
